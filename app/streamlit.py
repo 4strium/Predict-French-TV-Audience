d711ebdc54144dfd5fdb1319f7bb8c1367ddcf4a
Astrium <romain.mellaza@orange.fr>
2026-01-11 18:00:50 +0100

    Refactor prediction output formatting for improved readability in Streamlit app

--- app/streamlit.py
+++ app/streamlit.py
@@ -155,4 +155,5 @@ if st.button("Predict !") :
   # Combiner toutes les features des nouvelles données
   input_data_final = np.hstack([input_data_scaled, input_data_encoded.toarray(), input_data[['Vacances scolaires']].values, genres_encoded, nationalite_encoded])
   st.subheader("Model prediction for your film :")
-  st.write(f"Le film {film_to_predict['TITRE']} diffusé sur {film_to_predict['Chaîne']}, le {date_diffusion} peut espérer une audience de {round(model.predict(input_data_final)[0], 5)} millions de téléspectateurs.")
+  prediction = float(model.predict(input_data_final)[0])
+  st.write(f"Le film {film_to_predict['TITRE']} diffusé sur {film_to_predict['Chaîne']}, le {date_diffusion} peut espérer une audience de {round(prediction, 3)} millions de téléspectateurs.")
